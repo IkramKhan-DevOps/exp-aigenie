f7891be82dc79b1bdba6d99e11a7d76828e5cc6e
Ikram Khan <ikram.khan0762@gmail.com>
2023-02-09 18:31:48 +0500

    DBMS: admins app new models added

--- src/administration/admins/admin.py
+++ src/administration/admins/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import (
+    Package, Purchase
+)
 
-# Register your models here.
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'tokens', 'created_on', 'is_active']
+
+
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ['user', 'package', 'amount_total', 'amount_paid', 'tokens', 'created_on', 'is_active']
+
+
+admin.site.register(Package, PackageAdmin)
+admin.site.register(Purchase, PurchaseAdmin)
---
+++ src/administration/admins/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+from src.accounts.models import User
+
+
+class Package(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.FloatField()
+    tokens = models.PositiveIntegerField()
+
+    is_active = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.name
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, related_name='users', on_delete=models.CASCADE)
+    package = models.ForeignKey(Package, related_name='packages', on_delete=models.SET_NULL, null=True, blank=False)
+    amount_total = models.FloatField()
+    amount_paid = models.FloatField()
+    tokens = models.PositiveIntegerField()
+
+    is_active = models.BooleanField(default=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on', 'package']
+
+    def __str__(self):
+        return str(self.pk)
+
