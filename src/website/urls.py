90ea6a87e634cd601892e1a1951fe6d6218a03c6
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2023-02-10 17:05:07 +0500

    FEAT: app download backend added

--- src/website/urls.py
+++ src/website/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (
-    HomeView, PrivacyPolicyView, TermsAndConditionsView
+    HomeView, PrivacyPolicyView, TermsAndConditionsView, download_app
 )
 
 app_name = "website"
@@ -9,4 +9,5 @@ urlpatterns = [
     path('privacy-policy/', PrivacyPolicyView.as_view(), name='privacy-policy'),
     path('terms-and-conditions/', TermsAndConditionsView.as_view(), name='terms-and-conditions'),
 
+    path('download/app/', download_app, name='download_app'),
 ]
--- src/website/views.py
+++ src/website/views.py
@@ -21,4 +21,24 @@ class TermsAndConditionsView(TemplateView):
     template_name = 'website/terms-and-conditions.html'
 
 
+def download_app(request):
 
+    # CHECK: if requested version is available or not
+    app = ApplicationSoftware.objects.filter().order_by('-version')
+
+    if app:
+
+        # STATISTICS: update statistics
+        app = app[0]
+        app.total_downloads += 1
+        app.save()
+
+        # SAVE: download settings
+        file = app.app_file
+        file_extension = str(file).split('.')[1]
+        file_name = f"{app.name} - {app.version}.{file_extension}"
+        response = FileResponse(file)
+        response['Content-Disposition'] = f'attachment; filename="{file_name}"'
+
+        return response
+    raise Http404
