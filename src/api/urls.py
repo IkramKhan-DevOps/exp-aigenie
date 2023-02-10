c65a36c205040e1e3f6e24bfcd5c7948874f1548
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2023-02-10 18:12:07 +0500

    FEAT: API profile added

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,17 +2,17 @@ from django.urls import re_path, path
 
 from src.api.views import (
     OpenAIChatAPi, PurchaseRetrieveAPIView, PurchaseListAPIView, PurchaseCreateAPIView,
-    PackageListAPIView
+    PackageListAPIView, ProfileRetrieveView
 )
 
 app_name = "api"
-# Api Urls
 urlpatterns = [
     re_path('chat-with-ai/', OpenAIChatAPi.as_view(), name="open-ai"),
 
     path('purchase/', PurchaseListAPIView.as_view(), name="purchase"),
     path('purchase/add/', PurchaseCreateAPIView.as_view(), name="purchase-add"),
     path('purchase/<int:pk>/', PurchaseRetrieveAPIView.as_view(), name="purchase-detail"),
+    path('who-am-i/', ProfileRetrieveView.as_view(), name="profile-details"),
 
     path('package/', PackageListAPIView.as_view(), name="package"),
 ]
